eceb7e1ec89b978d5fba290c7e38864f16261745
unknown <ugasdomingo@gmail.com>
2023-08-21 18:09:52 +0200

    Improving courses info

--- frontend/src/static/coursesInfo.ts
+++ frontend/src/static/coursesInfo.ts
@@ -1,10 +1,10 @@
 export const coursesInfo = [
     {
         id: 1,
-        name: 'Modelación Labial con Ácido Hialurónico',
+        name: 'Mini Lipo Escultura con Técnica Endolifting',
         description:
-            'Te sumergirás en el mundo de la estética labial y aprenderás las técnicas más avanzadas para lograr resultados espectaculares. Nuestro equipo de expertos te guiará a través de cada paso del proceso, desde la consulta inicial hasta la aplicación precisa de ácido hialurónico de alta calidad.',
-        image: 'https://res.cloudinary.com/minteados/image/upload/v1692375287/Cursos/sofbfspkkxsj0tmymrn8.webp',
+            'Un procedimiento que redefine la forma en que abordamos la eliminación definitiva de la grasa en áreas problemáticas. Con experiencia práctica a través de videos de casos clínicos y la potencia del Láser Diodo de Fibra Óptica. Incluye la posibilidad de adquirir nuestro último equipo láser, diseñado para proporcionar reafirmación y efecto lipólisis de manera integral.',
+        image: 'https://res.cloudinary.com/minteados/image/upload/v1692375030/Cursos/yddvji5ej3evntf2tj3u.webp',
         location: 'Online',
         whyUs: 'Nuestra Técnica Mela no solo te proporcionará el conocimiento necesario para transformar vidas a través de procedimientos estéticos avanzados, sino que también te ofrecemos la oportunidad de mantener tu práctica actualizada y a la vanguardia. Con años de experiencia en el campo y un enfoque en la excelencia, te brindamos la confianza para destacarte en un mercado competitivo',
         benefits: [
@@ -49,10 +49,10 @@ export const coursesInfo = [
         id: 2,
         name: 'Rinomodelación no quirúrgica',
         description:
-            'Te sumergirás en el fascinante mundo de la transformación facial sin cirugía. Aprenderás las técnicas más avanzadas para esculpir y dar forma a la nariz de manera segura y efectiva. Nuestro equipo de expertos te guiará a través de cada paso del proceso, desde la evaluación inicial hasta la aplicación precisa de rellenos dérmicos de última generación.',
+            'Aprenderás las técnicas más avanzadas para esculpir y dar forma a la nariz de manera segura y efectiva, sin cirugía. Nuestro equipo de expertos te guiará a través de cada paso del proceso, desde la evaluación inicial hasta la aplicación precisa de rellenos dérmicos de última generación.',
         image: 'https://res.cloudinary.com/minteados/image/upload/v1692375224/Cursos/tikfj70lolutbxzsyr9g.webp',
         location: 'Online',
-        whyUs: 'Nuestro curso de rinomodelación no quirúrgica no solo te brindará habilidades técnicas de vanguardia, sino que también te empoderará para ofrecer a tus pacientes una solución estética sin cirugía, con resultados excepcionales. Conviértete en un experto en la creación de perfiles nasales elegantes y armoniosos, y abre las puertas a un enfoque más seguro y menos invasivo para realzar la belleza facial. Únete a nosotros y descubre cómo la rinomodelación no quirúrgica puede transformar tu práctica y la vida de tus pacientes.',
+        whyUs: 'Una solución estética sin cirugía, con resultados excepcionales. Conviértete en un experto en la creación de perfiles nasales elegantes y armoniosos, y abre las puertas a un enfoque más seguro y menos invasivo para realzar la belleza facial. Únete a nosotros y descubre cómo la rinomodelación no quirúrgica puede transformar tu práctica y la vida de tus pacientes.',
         benefits: [
             {
                 id: 1,
@@ -95,8 +95,8 @@ export const coursesInfo = [
         id: 3,
         name: 'Modelación labial con ácido hialurónico',
         description:
-            'Esta técnica es un procedimiento médico ambulatorio. Donde se capacitará al profesional de salud por medio de videos prácticos de casos clínicos a  eliminar la grasa localizada en pequeñas zonas de forma definitiva, Combinada con Láser Diodo de Fibra Óptica.',
-        image: 'https://res.cloudinary.com/minteados/image/upload/v1692375030/Cursos/yddvji5ej3evntf2tj3u.webp',
+            'Nuestra técnica avanzada de modelación labial con ácido hialurónico te brinda la habilidad de crear labios que cautiven la atención y reflejen la personalidad única de cada individuo, sin los inconvenientes ni el tiempo de recuperación asociados con cirugías invasivas. Prepárate para transformar sonrisas y elevar la confianza con este enfoque seguro y eficaz.',
+        image: 'https://res.cloudinary.com/minteados/image/upload/v1692375287/Cursos/sofbfspkkxsj0tmymrn8.webp',
         location: 'Online',
         whyUs: 'Nuestro curso de modelación labial con ácido hialurónico te brindará no solo habilidades técnicas excepcionales, sino también la capacidad de transformar sonrisas y elevar la confianza de tus pacientes de manera significativa. Conviértete en un experto en la creación de labios elegantes y naturales que reflejen la personalidad única de cada individuo. Únete a nosotros y descubre cómo la modelación labial con ácido hialurónico puede elevar tu práctica a nuevas alturas estéticas y emocionales.',
         benefits: [
